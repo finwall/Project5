23d4819a60a8c22a03d8b1532e9ebcfa0f5cf022
sms10 <sms10@iu.edu>
2022-10-11 18:33:27 -0400

    Search page now receives search params properly

--- front-end/src/components/screens/search.jsx
+++ front-end/src/components/screens/search.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import SearchService from '../../services/search';
 
 export default function Search(props) {
 
-    // const [searchParams, setSearchParams] = useState(new URLSearchParams(props.location?.search));
-    const [searchParams, setSearchParams] = useState(props.location?.search);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    
+    console.log(searchParams.get('search'));
 
     return (
         <>
             <h1>This is the search page.</h1>
             <p>Data received:</p>
             <blockquote>
-                {searchParams}
+                {searchParams.getAll('search')}
             </blockquote>
         </>
     )
