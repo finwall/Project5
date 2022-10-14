f0865bdb53746287de6f5c4cab20a9e1770be2b5
sms10 <sms10@iu.edu>
2022-10-14 04:51:45 -0400

    Added security questions

--- front-end/src/components/form-loginSignup.jsx
+++ front-end/src/components/form-loginSignup.jsx
@@ -8,6 +8,8 @@ export default function LoginSignupForm({ isSignup }) {
     const [nameInput, setNameInput] = useState("");
     const [emailInput, setEmailInput] = useState("");
     const [passwordInput, setPasswordInput] = useState("");
+    const [secQuestion1, setSecQuestion1] = useState("");
+    const [secQuestion2, setSecQuestion2] = useState("");
     const [errorMessages, setErrorMessages] = useState([]);
     const [successMsg, setSuccessMsg] = useState("");
 
@@ -23,6 +25,14 @@ export default function LoginSignupForm({ isSignup }) {
 
     function handlePasswordChange(e) {
         setPasswordInput(e.target.value);
+    }
+    
+    function handleSQ1Change(e) {
+        setSecQuestion1(e.target.value);
+    }
+    
+    function handleSQ2Change(e) {
+        setSecQuestion2(e.target.value);
     }
 
     function handleSubmitError(e) {
@@ -56,7 +66,6 @@ export default function LoginSignupForm({ isSignup }) {
             let loginPromise = AuthService.login(emailInput, passwordInput)
                 .then(
                     () => {
-                        console.log("Successfully logged in!");
                         setSuccessMsg(`You have successfully logged in, ${emailInput}!`);
                     })
             loginPromise.catch(e => handleSubmitError(e))
@@ -74,6 +83,7 @@ export default function LoginSignupForm({ isSignup }) {
     // whether the user is accessing this form from the signup page
     let newAccountField = null;
     let nameFields = null;
+    let securityQuestions = null;
 
     // generates the additional JSX
     if (!isSignup) {
@@ -92,6 +102,19 @@ export default function LoginSignupForm({ isSignup }) {
                     <input type="text" name="name" value={nameInput} onChange={handleNameChange} autoComplete={(isSignup) ? "new-password" : "off"} />
                 </label>
             </div>
+        );
+        securityQuestions = (
+            <>
+                <h2>Security Questions</h2>
+                <label>
+                    What was your mother's maiden name?
+                    <input type="text" name="sq1" value={secQuestion1} onChange={handleSQ1Change} autoComplete={(isSignup) ? "new-password" : "off"} />
+                </label>
+                <label>
+                    What was the name of your first pet?
+                    <input type="text" name="sq2" value={secQuestion2} onChange={handleSQ2Change} autoComplete={(isSignup) ? "new-password" : "off"} />
+                </label>
+            </>
         );
     }
 
@@ -120,6 +143,7 @@ export default function LoginSignupForm({ isSignup }) {
                     Password
                     <input type="password" name="Password" value={passwordInput} onChange={handlePasswordChange} autoComplete={(isSignup) ? "new-password" : "off"} />
                 </label>
+                {securityQuestions}
                 <input type="submit" value="Submit" />
             </form>
             {newAccountField}
