c763e0912748c3ce2255c9b38519592bc0b55b9e
finwall <griwall@iu.edu>
2022-10-15 21:37:26 -0400

    Commit

--- front-end/src/services/city.js
+++ front-end/src/services/city.js
@@ -4,12 +4,10 @@ const GET_CITY_API ="";
 class CityService{
         verifyCity(city,location) {
 
-        let returnArray = [];
-
         /*
         // enable code block when endpoint created
         return axios
-            .post(VERIFY_API, {
+            .get(VERIFY_API, {
                 city,
                 location
             })
@@ -24,8 +22,26 @@ class CityService{
             resolve()
         })
         }
+        getCityInfo(city,location) {
 
-
+        /*
+        // enable code block when endpoint created
+            return axios
+                .get(VERIFY_API, {
+                    city,
+                    location
+             })
+            .then(response => {
+                return null
+            })
+            .catch(e => {
+                return null
+            })
+            */
+        return new Promise((resolve, reject) => {
+            resolve()
+        })
+        }
 }
 
 export default new CityService()
