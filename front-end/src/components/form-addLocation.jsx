9be2d84833ba7c9430b79fa69330d3761fa1e8f1
sms10 <sms10@iu.edu>
2022-10-30 01:57:01 -0400

    Created new route, prepared trip planning page

---
+++ front-end/src/components/form-addLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchForm from 'form-search.jsx';
+
+
+/**
+ * 
+ * @param elementNumber 
+ */
+function AddLocation({elementNumber, xClicked, fromLocationName, fromLocationID}) {
+    return ( 
+        <div>
+            <SearchForm></SearchForm>
+        </div>
+    );
+}
+
+export default AddLocation;
---
+++ front-end/src/components/screens/create-new-itinerary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageWrapper from "./wrappers/wrapper-regularPage";
+
+function CreateItinerary() {
+    return ( 
+        <PageWrapper>
+            <h1>Hello there</h1>
+        </PageWrapper>
+    );
+}
+
+export default CreateItinerary;
