133861c499d64693576f879893e58661efc268b0
sms10 <sms10@iu.edu>
2022-10-30 16:04:40 -0400

    Completed stage 1,2 from addlocation form

--- front-end/src/components/form-addLocation.jsx
+++ front-end/src/components/form-addLocation.jsx
@@ -1,15 +1,124 @@
-import React from "react";
-import SearchForm from 'form-search.jsx';
+import { React, useState, useEffect } from "react";
+import SearchForm from './form-search.jsx';
+
+import Styles from './css/form-addLocation.module.css'
+import { useCallback } from "react";
 
 
 /**
- * 
- * @param elementNumber 
+ * JSX component for providing a form for the user to add a travel location, hotel, and ammenities
+ * @param {object} props
+ * @param {int} props.id A simple number to show the order of this location in the travel list
+ * @param {() => void} props.xClicked A function that is executed when the user clicks the X button in this component
+ * @param {string} props.fromLocationName The name of a previous location (usually used for location chaining)
+ * @param {string} props.fromLocationID The id of a previous location (usually used for location chaining)
+ * @param {string} props.toLocationName The name of the new location
+ * @param {string} props.toLocationID The id of the new location
+ * @param {string} props.rootCollectFromData Function allowing parent to collect "from location" data
+ * @param {string} props.rootCollectToData Function allowing parent to collect "to location" data
  */
-function AddLocation({elementNumber, xClicked, fromLocationName, fromLocationID}) {
+function AddLocation({id, xClicked, fromLocationName, fromLocationID, toLocationName, toLocationID, rootCollectFromData, rootCollectToData}) {
+
+    // const [ fromName, setFromName ] = useState(fromLocationName || "")
+    // const [ fromID, setFromID ] = useState(fromLocationID || "")
+    // const [ toName, setToName ] = useState("")
+    // const [ toID, setToID ] = useState("")
+
+    let fromName = fromLocationName || "";
+    let toName = toLocationName || "";
+
+    // solution from Salman A,
+    // https://stackoverflow.com/a/13627586/15818885
+    function ordinal_suffix_of(i) {
+        var j = i % 10,
+            k = i % 100;
+        if (j === 1 && k !== 11) {
+            return i + "st";
+        }
+        if (j === 2 && k !== 12) {
+            return i + "nd";
+        }
+        if (j === 3 && k !== 13) {
+            return i + "rd";
+        }
+        return i + "th";
+    }
+
+    function Stage01(props) {
+        return (
+            <>
+                <SearchForm
+                    placeholderSupplement={`Select ${ordinal_suffix_of(id)} location`}
+                    selectItemAction={collectToData}
+                    clearSearch={true}
+                ></SearchForm>
+            </>
+        )
+    } 
+
+    const Stage02 = useCallback((props) => {
+        return (
+            <>
+                <Stage01 />
+                <div className={Styles['flights']}>
+                    <h2>{toName}</h2>
+                    <div className={Styles['map']}>
+                        MAP WOULD GO HERE
+                    </div>
+                    <div className={Styles['flightsSearch']}>
+                        <h3>Flights</h3>
+                        <SearchForm
+                            placeholderSupplement={`From`}
+                            selectItemAction={collectFromData}
+                            clearSearch={true}
+                        ></SearchForm>
+                    </div>
+                </div>
+            </>
+        )
+    }, [toName]);
+
+    const Stage03 = useCallback((props) => {
+        return (
+            <>
+                <Stage02 />
+                <h2>{fromName}</h2>
+                {/* <div className={Styles['flightsSearch']}>
+                    <h2>{fromName}</h2>
+                    <h3>Flights</h3>
+                    <SearchForm
+                        placeholderSupplement={`From`}
+                        selectItemAction={collectFromData}
+                        clearSearch={true}
+                    ></SearchForm>
+                </div> */}
+            </>
+        )
+    }, [fromName]);
+
+    const [stage, setStage] = useState(<Stage01 />);
+
+    useEffect(() => {console.log(stage)}, [stage])
+
+    function collectToData({ locationName, locationLocation }) { // executed when the user searches for the next location
+        rootCollectToData({locationName, locationLocation});
+        toName = locationName;
+        setStage(<Stage02 />);
+    }
+    
+    function collectFromData({ locationName, locationLocation }) { // executed when the user searches for a previous location
+        fromName = locationName;
+        rootCollectFromData({locationName, locationLocation}); // should set this element's props
+        setStage(<Stage03 />);
+    }
+
     return ( 
-        <div>
-            <SearchForm></SearchForm>
+        <div className={Styles['addLocation']}>
+            <div className={Styles['locationHeader']}>
+                <h2>Location {id}</h2>
+                <button className={Styles['x']} onClick={xClicked}>X</button>
+            </div>
+            {stage}
         </div>
     );
 }
--- front-end/src/components/screens/create-new-itinerary.jsx
+++ front-end/src/components/screens/create-new-itinerary.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import PageWrapper from "./wrappers/wrapper-regularPage";
+import Location from '../form-addLocation';
+import { useState } from "react";
 
 function CreateItinerary() {
+
+    const [locationData, setLocationData] = useState([
+        {fromLocationName: null, fromLocationID: null, toLocationName: null, toLocationID: null}
+    ]);
+
+
+    function collectToData({ locationName, locationLocation }, elementIndex) {
+        let currentLocationData = locationData.map((item) => {return item});
+        currentLocationData[elementIndex].fromLocationName = locationName;
+        currentLocationData[elementIndex].fromLocationID = locationLocation;
+        setLocationData(currentLocationData);
+        // console.log("updated ", locationData, "with ", currentLocationData)
+    }
+
+    function collectFromData({ locationName, locationLocation }, elementIndex) {
+        let currentLocationData = locationData.map((item) => { return item });
+        currentLocationData[elementIndex].toLocationName = locationName;
+        currentLocationData[elementIndex].toLocationID = locationLocation;
+        setLocationData(currentLocationData);   
+        // console.log("updated ", locationData, "with ", currentLocationData)
+    }
+
+
     return ( 
         <PageWrapper>
-            <h1>Hello there</h1>
+            {
+                locationData.map((location, index) => {
+                    return (
+                        <div style={{ display: "flex", flexDirection: "row" }}>
+                            <Location
+                                id={index+1}
+                                fromLocationName={location.fromLocationName}
+                                fromLocationID={location.fromLocationID}
+                                toLocationName={location.toLocationName}
+                                toLocationID={location.toLocationID}
+                                rootCollectToData={({locationName, locationLocation}) => {collectToData({locationName, locationLocation}, index)}}
+                                rootCollectFromData={({locationName, locationLocation}) => {collectFromData({locationName, locationLocation}, index)}}
+                            ></Location>
+                        </div>
+                    )
+                })
+            }
         </PageWrapper>
     );
 }
